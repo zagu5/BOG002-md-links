75dcb6cc5e2d48009436333665c7e4eb81660e2f
zagu5 <zury.agudelo@gmail.com>
2021-08-02 09:20:52 -0500

    Se hace funcion readDir y readFile

--- archivos/prueba.md
+++ archivos/prueba.md
@@ -1,2 +1,6 @@
 # Markdown Links
+## Docuemnto de prueba
 [Markdown](https://es.wikipedia.org/wiki/Markdown)
+![md-links](Diagrama.png)
+
+
--- index.js
+++ index.js
@@ -2,8 +2,55 @@
 //   // ...
 // };
 const path = require('path');
+const fs = require('fs');
+const { resolve } = require('path');
 
-//  const ruta = ('/prueba.md')
+const routeFile = ('C:\\BOG002-md-links\\archivos\\prueba.md');
+const routeDir = ('C:\\BOG002-md-links\\archivos')
 
-const absolute = path.isAbsolute('//prueba.md');
-console.log('la ruta es ' + absolute)
+const absolute = path.isAbsolute(routeFile);
+// console.log('la ruta es ' + absolute)
+
+
+function readFiles(route){
+  return new Promise ((resolve,reject) => {
+    fs.readFile(route, 'utf8', function(err, data) {
+      if (err) {
+        return reject(err);
+      }
+      resolve(data);
+    });
+  })
+}
+
+readFiles(routeFile)
+.then((response)=>{
+  console.log(response)
+})
+.catch((err)=>{
+  console.log(err)
+})
+
+// const extFile = path.extname(routeFile);
+// console.log('el archivo es de extension'+ extFile)
+
+function readDirectory(route){
+  return new Promise((resolve, reject)=>{
+    fs.readdir(route, 'utf-8', function(err,files){
+      if (err) {
+        return reject(err);
+      }
+      resolve(files);
+    })
+  })
+}
+
+readDirectory(routeDir)
+.then((response)=>{
+  console.log(response)
+})
+.catch((err)=>{
+  console.log(err)
+})
+
+
